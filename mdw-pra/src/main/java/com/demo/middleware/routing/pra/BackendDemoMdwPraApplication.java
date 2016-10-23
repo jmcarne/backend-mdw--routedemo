7fffad75677b6191d59f8d492d31941e61d139bb
Josep Maria <josep_maria_carne@hotmail.com>
2016-10-23 04:26:02 +0200

    Initial commit pra domain

---
+++ mdw-pra/src/main/java/com/demo/middleware/routing/pra/BackendDemoMdwPraApplication.java
@@ -0,0 +1,13 @@
+package com.demo.middleware.routing.pra;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Josep Maria on 23/10/2016.
+ */
+@SpringBootApplication
+public class BackendDemoMdwPraApplication {
+
+    public static void main(String args[]) {
+    }
+}
