81dddc77cfc425566dd37902ba9cbc0c9aab95fe
Josep Maria <josep_maria_carne@hotmail.com>
2016-10-23 04:26:24 +0200

    Initial commit pra domain

---
+++ mdw-pra/src/main/java/com/demo/middleware/routing/pra/usecase/MainRouteBuilder.java
@@ -0,0 +1,15 @@
+package com.demo.middleware.routing.pra.usecase;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * Created by Josep Maria on 23/10/2016.
+ */
+public class MainRouteBuilder extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
+
