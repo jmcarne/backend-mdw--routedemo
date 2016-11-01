116fcf59166798c03275c72542bd053e5a2f2763
Josep Maria <josep_maria_carne@hotmail.com>
2016-11-01 03:29:36 +0100

    Initial commit pra domain

--- mdw-pra/src/main/java/com/demo/middleware/routing/pra/BackendDemoMdwPraApplication.java
+++ mdw-pra/src/main/java/com/demo/middleware/routing/pra/BackendDemoMdwPraApplication.java
@@ -1,5 +1,6 @@
 package com.demo.middleware.routing.pra;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -9,5 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendDemoMdwPraApplication {
 
     public static void main(String args[]) {
+        SpringApplication.run(SpringApplication.class, args);
     }
 }
